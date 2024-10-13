b114c9cda733afeea41053d063c0fffd320d2457
Mortezazadeh <kh.mortezazade@gmail.com>
2024-10-13 20:09:17 +0330

    Add files via upload

---
+++ tam3.cs
@@ -0,0 +1,17 @@
+﻿// tamrin
+    for (; ; )
+    {
+        Console.WriteLine("please enter mobile number:");
+        string mobile = Console.ReadLine();
+        if (mobile.Length == 11 && mobile.StartsWith("0"))
+        {
+               int mobile_m = int.Parse(mobile);
+                Console.WriteLine("inter correct mobile number.");
+                break;
+        }
+        else
+        {
+            Console.WriteLine("mobile number is wrong.");
+        }
+
+    }
