f1e7cdf66a2ff8db4b2803a60069adc394f7b9fb
Mortezazadeh <kh.mortezazade@gmail.com>
2024-10-27 18:40:13 +0330

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -3,54 +3,66 @@
 
 using System.ComponentModel.Design;
 
-Console.WriteLine("please enter your first name");
-string? FirstName = Console.ReadLine();
-Console.WriteLine("please enter your last name");
-string LastName = Console.ReadLine() ?? "";
 
-do
-{
-    Console.ForegroundColor =ConsoleColor.White;
-    Console.WriteLine("Please enter your age");
-    int age = int.Parse(Console.ReadLine() ?? "");
-    if (age < 18 || age>120)
-    {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("You are not valid to register");
-    }
-    else
-    {
-        Console.WriteLine("You can register");
-        break;
-    }
-} while (true);
+student student = new();
+student.firstNmae = "khm";
+student.lastNmae = "MRTZ";
+student.age = 30;
+student.phoneNumber = 09122117955;
 
 
-do 
-{
-    Console.ForegroundColor = ConsoleColor.White;
-    Console.WriteLine("Please ente your phone number:");
-    string phoneNumber = Console.ReadLine() ?? string.Empty;
-    if ((phoneNumber.Length == 11 && phoneNumber.StartsWith("0")) || (phoneNumber.Length == 10 && !phoneNumber.StartsWith("0")))
-        {
-            try
-            {
-            long phone = long.Parse(phoneNumber);
-            Console.WriteLine("correct phone number.");
-            break;
-            }
-            catch
-            {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Invalid phone number.");
-            }
-        }
-    else
-        {
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine("Invalid phone number.");
-        }
-} while(true);
+
+
+
+
+//Console.WriteLine("please enter your first name");
+//string? FirstName = Console.ReadLine();
+//Console.WriteLine("please enter your last name");
+//string LastName = Console.ReadLine() ?? "";
+
+//do
+//{
+//    Console.ForegroundColor =ConsoleColor.White;
+//    Console.WriteLine("Please enter your age");
+//    int age = int.Parse(Console.ReadLine() ?? "");
+//    if (age < 18 || age>120)
+//    {
+//        Console.ForegroundColor = ConsoleColor.Red;
+//        Console.WriteLine("You are not valid to register");
+//    }
+//    else
+//    {
+//        Console.WriteLine("You can register");
+//        break;
+//    }
+//} while (true);
+
+
+//do 
+//{
+//    Console.ForegroundColor = ConsoleColor.White;
+//    Console.WriteLine("Please ente your phone number:");
+//    string phoneNumber = Console.ReadLine() ?? string.Empty;
+//    if ((phoneNumber.Length == 11 && phoneNumber.StartsWith("0")) || (phoneNumber.Length == 10 && !phoneNumber.StartsWith("0")))
+//        {
+//            try
+//            {
+//            long phone = long.Parse(phoneNumber);
+//            Console.WriteLine("correct phone number.");
+//            break;
+//            }
+//            catch
+//            {
+//            Console.ForegroundColor = ConsoleColor.Red;
+//            Console.WriteLine("Invalid phone number.");
+//            }
+//        }
+//    else
+//        {
+//        Console.ForegroundColor = ConsoleColor.Red;
+//        Console.WriteLine("Invalid phone number.");
+//        }
+//} while(true);
 
 
 
---
+++ studentClass.cs
@@ -0,0 +1,9 @@
+
+internal class student
+{
+    public string firstNmae { get; set; }
+    public string lastNmae { get; set; }
+    public int age { get; set; }
+    public long phoneNumber {  get; set; }
+    }
+ 
